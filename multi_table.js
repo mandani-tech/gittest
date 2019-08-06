8a07681b62f829f2b98535adb8614373cda78fd2
mandani-tech <mandani.hamida@gmail.com>
2019-08-06 10:53:36 -0500

    multiplication using a number

---
+++ multi_table.js
@@ -0,0 +1,7 @@
+alert("Lets make a multiplication table here");
+var table=prompt("Please enter a number here:");
+for(var a=1;a<=10;a++)
+{
+	document.write('\xa0\xa0\xa0\xa0\xa0\xa0\xa0 '  +   table  + " * "  +  a  +  " = " + '\xa0'+ (a*table)+"<br/>"); 
+
+}	
